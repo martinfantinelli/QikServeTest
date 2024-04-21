e3d930c0f3248e823de83a3238ecc0f408ae5168
martinfantinelli <martinsfantinelli@gmail.com>
2024-04-21 17:05:06 -0300

    feat: initial config.

---
+++ src/main/java/org/example/qikservetest/qikserve/QikServeApplication.java
@@ -0,0 +1,13 @@
+package org.example.qikservetest.qikserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QikServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QikServeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/qikservetest/qikserve/QikServeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.qikservetest.qikserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QikServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
